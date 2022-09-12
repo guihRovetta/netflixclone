335a2efc03894ee5b50f95c235ec7919fb111e29
guihRovetta <guilherme020213@hotmail.com>
2022-09-12 14:12:43 -0300

    fix: adjust eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,22 @@
 module.exports = {
   env: {
-    es6: true,
-    node: true,
-    jest: true
+    browser: true,
+    es2021: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    'plugin:@typescript-eslint/recommended'
   ],
   overrides: [
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
   }
 }
