94016bcf624c6cfe3056b7842909d4a795e24793
guihRovetta <guilherme020213@hotmail.com>
2022-09-12 15:26:45 -0300

    feat: install react-navigation package

--- android/app/src/main/java/com/netflixclone/MainActivity.java
+++ android/app/src/main/java/com/netflixclone/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+// react-native-screens
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -23,6 +26,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  // react-native-screens
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
