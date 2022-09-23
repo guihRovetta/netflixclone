98e9a0e0098f036029e8033ba63e7f64fe202a7a
guihRovetta <guilherme020213@hotmail.com>
2022-09-23 15:16:18 -0300

    feat: create ParentalRatingIndicator component

---
+++ src/components/ParentalRatingIndicator/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import {Box, Text} from '@internal/components';
+import {Theme} from '@internal/themes';
+
+const PARENTAL_RATING_OBJ: Record<
+  string,
+  {color: keyof Theme['colors']; label: string}
+> = {
+  all: {color: 'green', label: 'L'},
+  10: {color: 'blue', label: '10'},
+  12: {color: 'yellow', label: '12'},
+  14: {color: 'orange', label: '14'},
+  16: {color: 'red', label: '16'},
+  18: {color: 'black', label: '18'},
+};
+
+const DEFAULT_SQUARE_SIZE = 36;
+
+export const ParentalRatingIndicator = ({value}: {value: string}) => (
+  <Box
+    width={DEFAULT_SQUARE_SIZE}
+    height={DEFAULT_SQUARE_SIZE}
+    alignItems="center"
+    p={2}
+    bg={PARENTAL_RATING_OBJ[value]?.color}
+    borderRadius={4}
+    borderWidth={1}
+    borderColor="white">
+    <Text color={value === '12' ? 'black' : 'white'} fontWeight="600">
+      {PARENTAL_RATING_OBJ[value]?.label}
+    </Text>
+  </Box>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './Box';
 export * from './Button';
 export * from './Icon';
+export * from './ParentalRatingIndicator';
 export * from './Text';
