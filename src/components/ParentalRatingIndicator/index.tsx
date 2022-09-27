fb8e2ceb703f7d38c52a2cf3fd7d49218afbc9e0
guihRovetta <guilherme020213@hotmail.com>
2022-09-27 16:25:27 -0300

    fix: adjust style and add container props

--- src/components/ParentalRatingIndicator/index.tsx
+++ src/components/ParentalRatingIndicator/index.tsx
@@ -4,12 +4,12 @@ import {Box, Text} from '@internal/components';
 import {Theme} from '@internal/themes';
 import {Item} from '@internal/server/mirage-js';
 
-const DEFAULT_SQUARE_SIZE = 36;
+const DEFAULT_SQUARE_SIZE = 30;
 const PARENTAL_RATING_OBJ: Record<
   string,
   {color: keyof Theme['colors']; label: Item['parentalRating']}
 > = {
-  all: {color: 'green', label: 'L'},
+  L: {color: 'green', label: 'L'},
   10: {color: 'blue', label: '10'},
   12: {color: 'yellow', label: '12'},
   14: {color: 'orange', label: '14'},
@@ -17,16 +17,23 @@ const PARENTAL_RATING_OBJ: Record<
   18: {color: 'black', label: '18'},
 };
 
-export const ParentalRatingIndicator = ({value}: {value: string}) => (
+export const ParentalRatingIndicator = ({
+  _containerProps,
+  value,
+}: {
+  _containerProps?: React.ComponentProps<typeof Box>;
+  value: string;
+}) => (
   <Box
     width={DEFAULT_SQUARE_SIZE}
     height={DEFAULT_SQUARE_SIZE}
     alignItems="center"
-    p={2}
+    p={1}
     bg={PARENTAL_RATING_OBJ[value]?.color}
     borderRadius={4}
     borderWidth={1}
-    borderColor="white">
+    borderColor="white"
+    {..._containerProps}>
     <Text color={value === '12' ? 'black' : 'white'} fontWeight="600">
       {PARENTAL_RATING_OBJ[value]?.label}
     </Text>
